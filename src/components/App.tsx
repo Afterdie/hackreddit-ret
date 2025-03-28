90886a6f12baa6c465c0a07f5ab5a38353187359
Akshat <82810411+Afterdie@users.noreply.github.com>
2025-03-28 16:57:03 +0530

    [feat] added logic for preFitness storing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -15,19 +15,20 @@ const App: Devvit.CustomPostComponent = (context: Devvit.Context) => {
   //loads in the creature for the current post
   const { reddit, redis, media, ui } = context;
   const postId = context.postId;
+
+  const [gene, setGene] = useState<number | null>(null);
+
   const {
     data: mediaUrl,
     loading,
     error,
   } = useAsync(async () => {
-    //const res = await redis.hGet("preFitness", `${postId}`);
     const mediaId = await redis.get(`image:${postId}`);
-    const mediaUrl = `https://i.redd.it/${mediaId}.png`;
-    return mediaUrl ?? null;
+    return mediaId ? `https://i.redd.it/${mediaId}.png` : null;
   });
 
   //generates a new post and stores its creature in media and mediaid in redis
-  const createPost = async (imageUrl: string): Promise<void> => {
+  const createPost = async (imageUrl: string): Promise<string> => {
     const userName = await reddit.getCurrentUsername();
     const subName = context.subredditName || "Some guy";
     const response = await media.upload({
@@ -39,12 +40,12 @@ const App: Devvit.CustomPostComponent = (context: Devvit.Context) => {
       subredditName: subName,
       preview: (
         <vstack>
-          <text>Wait a min</text>
+          <text>${userName || "Someone"}'s RedPet is hatching 🐣</text>
         </vstack>
       ),
     });
     await redis.set(`image:${post.id}`, response.mediaId);
-    redis.hSet("preFitness", { [post.id]: "1100" });
+    return post.id;
   };
 
   //for handling the webview interaction and messages
@@ -57,21 +58,29 @@ const App: Devvit.CustomPostComponent = (context: Devvit.Context) => {
 
     // Message handler
     onMessage: async (message) => {
-      if (message.type === "webViewReady") {
-        const gene = await createGene(context);
-        postMessage({
-          type: "set-gene",
-          gene: gene,
-        });
-      } else if (message.type === "imgURL") {
-        unmount();
-        try {
-          createPost(message.url);
+      try {
+        if (message.type === "webViewReady") {
+          const newGene = await createGene(context);
+          if (!newGene) throw new Error("Failed to generate gene");
+          setGene(newGene);
+          postMessage({
+            type: "set-gene",
+            gene: newGene,
+          });
+        } else if (message.type === "imgURL") {
+          if (!gene) throw new Error("Faulty Gene");
+          unmount();
+          //get the post id of the post and store the gene against the postid
+          const postId = await createPost(message.url);
+          //store the preFitness values used for generating the leaderboards
+          await redis.hSet("preFitness", { [postId]: gene.toString(2) });
           ui.showToast("Hatched your RedPet");
-        } catch (e) {
-          if (e instanceof Error) ui.showToast(`Failed: ${e.message}`);
-          else ui.showToast("Something went wrong");
         }
+      } catch (e) {
+        if (e instanceof Error) ui.showToast(`Failed: ${e.message}`);
+        else ui.showToast("Something went wrong");
+      } finally {
+        unmount();
       }
     },
 
